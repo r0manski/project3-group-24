0e3c202ee2fc3ed4cd51e645084ae2b021a7d2a6
muchness25 <37822155+muchness25@users.noreply.github.com>
2018-05-14 00:36:56 +1000

    Hoang Mai Tu
    Upload work files

---
+++ Reciper Keeper/Recipe.swift
@@ -0,0 +1,41 @@
+//
+//  Recipe.swift
+//  Reciper Keeper
+//
+//  Created by Alice Mai Tu on 13/5/18.
+//  Copyright © 2018 Alice Mai Tu. All rights reserved.
+//
+
+import Foundation
+
+protocol Ingredient {
+    var ingredient: [String: String] { get set}
+}
+
+struct Instruction {
+    var step: [Int: String]
+    var timer: [Int: Int]
+}
+
+enum Cuisine: String {
+    case Australian
+    case Chinese
+    case Italian
+    case Thai
+    case Vietnamese
+}
+
+class Recipe: Ingredient {
+    var name: String
+    var ingredient: [String : String]
+    var instruction: Instruction?
+    var time: Int
+    var cuisine: Cuisine
+    
+    init (_ name: String, _ ingredient: [String: String], _ time: Int, _ cuisine: Cuisine) {
+        self.name = name
+        self.ingredient = ingredient
+        self.time = time
+        self.cuisine = cuisine
+    }
+}
