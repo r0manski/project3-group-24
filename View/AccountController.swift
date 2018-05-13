b01b92518a5f4125455240fbe06f7ab8e1f18f27
Alice <muchness25@gmail.com>
2018-05-14 00:12:16 +1000

    Alice
    Alice's
    Basic UI and app structure

---
+++ View/AccountController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Reciper Keeper
+//
+//  Created by Alice Mai Tu on 10/5/18.
+//  Copyright © 2018 Alice Mai Tu. All rights reserved.
+//
+
+import UIKit
+
+class AccountController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
